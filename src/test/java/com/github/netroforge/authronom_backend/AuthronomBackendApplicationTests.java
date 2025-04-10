f6af0f4eab6d1b575d583525196c78c7f1367d31
Yevhen Tienkaiev <hronom@gmail.com>
2025-04-10 05:16:39 +0300

    Add base for tests

---
+++ src/test/java/com/github/netroforge/authronom_backend/AuthronomBackendApplicationTests.java
@@ -0,0 +1,14 @@
+package com.github.netroforge.authronom_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class AuthronomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
